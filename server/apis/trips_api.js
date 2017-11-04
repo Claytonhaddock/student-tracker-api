667caa10ee7cb7de5ba58a715111ecafb5433a84
Omar Patel <omarpatel123@gmail.com>
2017-11-04 16:17:53 -0700

    add university and cohort schemas

--- server/apis/trips_api.js
+++ server/apis/trips_api.js
@@ -1,25 +1,22 @@
 var Trip  = require('../models/Trip');
 
 exports.index = function(req, res) {
-	Trip
+  University
   .find()
-  .where('UserId').equals(req.user.id)
-  .then(function(dbTrip) {
-    res.render('trips/trips', {
-		  layout: 'main-trips',
-		  trip: dbTrip
-	  });
-  });
+  .where('id').equals(req.id)
+  .then(function(university){
+    res.json(university);
+  })
 };
 
-exports.createTrip = function(req, res) {
+// exports.createTrip = function(req, res) {
 
-	// Add id from User onto req.body
-	req.body.UserId = req.user.id;
+// 	// Add id from User onto req.body
+// 	req.body.UserId = req.user.id;
 
-  var newTrip = new Trip(req.body);
+//   var newTrip = new Trip(req.body);
 
-  newTrip.save().then(function(dbPost) {
-    res.json(dbPost);
-  });
-};
+//   newTrip.save().then(function(dbPost) {
+//     res.json(dbPost);
+//   });
+// };
---
+++ server/models/University.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var universitySchema = mongoose.Schema({
+
+    fullName: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 100,
+        required: [true, 'Please enter the name of the University']
+    },
+    shortName: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 50,
+    },
+    address: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 150,
+        required: [true, 'Please enter an address']
+    },
+    icon: {
+        type: String,
+        min: [1, 'Too few characters']
+    },
+    ongoingCohorts: [],
+    completedCohorts: [],
+    primaryColor: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 20,
+    }
+    secondaryColor: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 20,
+    },
+    cohorts: []
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('University', universitySchema);
