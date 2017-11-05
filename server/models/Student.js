1d139e0ce5d6612f33b6b0104412bfb04c62e621
Haddock, Clayton <chaddock@conversantmedia.com>
2017-11-04 17:24:46 -0700

    updated routes

---
+++ server/models/Student.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var schema   = mongoose.Schema;
+
+// define the schema for our user model
+var studentSchema = new schema({
+
+    name: {
+        type: String,
+        min: [1, 'Too few characters'],
+        max: 100,
+        required: [true, 'Please enter a name.']
+    },
+    sessions: [{
+        type: schema.ObjectId,
+        ref: 'Session'
+    }],
+    cohort: {
+        type: schema.ObjectId,
+        ref: 'Cohort'
+    }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Student', studentSchema);
