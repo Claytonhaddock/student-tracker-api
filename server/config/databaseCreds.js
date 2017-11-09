5df886c79041063f259f3667f19b399a0a4b1d85
Haddock, Clayton <chaddock@conversantmedia.com>
2017-11-08 21:23:38 -0800

    updated database creds

--- server/config/databaseCreds.js
+++ server/config/databaseCreds.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : process.env.MONGODB_URI || 'mongodb://127.0.0.1/hackathon-db-dev'
+    // 'url' : process.env.MONGODB_URI || 'mongodb://127.0.0.1/hackathon-db-dev'
+    'url' : process.env.MONGODB_URI || 'mongodb://username:password@ds031835.mlab.com:31835/student_tracker'
 
 };
